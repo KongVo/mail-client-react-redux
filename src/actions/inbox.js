f6c3914e94aa33af6e99888de7f80da0c018d6d4
Kong Vo <44124393+KongVo@users.noreply.github.com>
2022-06-20 21:55:38 -0700

    Add files via upload

--- src/actions/inbox.js
+++ src/actions/inbox.js
@@ -4,10 +4,13 @@ export const RECEIVE_API_DATA = "RECEIVE_API_DATA";
 
 
 export const requestApiData = () => ({ type: REQUEST_API_DATA });
-export const receiveApiData = data => ({ type: RECEIVE_API_DATA,payload: data });
+export const receiveApiData = (data) => 
+({ type: RECEIVE_API_DATA,
+  payload: data, });
 
 
 
 export const DELETE_INBOX_MAIL="DELETE_INBOX_MAIL";
-export const deleteApiData = id => ({ type: DELETE_INBOX_MAIL, payload:id });
+export const deleteApiData = (id) =>
+ ({ type: DELETE_INBOX_MAIL, payload:id });
 
